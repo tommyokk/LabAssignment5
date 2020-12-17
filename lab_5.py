62815ff8379765f83fba888425e6d0adfa61fb3d
Tommyok <tamokoro@gmail.com>
2020-12-16 23:43:58 -0800

    finish lab 5

---
+++ lab_5.py
@@ -0,0 +1,62 @@
+
+
+
+"""
+The program is trying to translate a sentence captured as user input.
+We first red in the text file textese.txt.
+then from the text file, we create a list of strings from each lines in the text file.
+Then, we create a dictionary from the list.
+Once the text file has been read into memory, we close the file.
+
+We then define a function for translating which involves splitting the user input into an 
+array of strings ("enjoy the excellent band tonight") ["enjoy", "the","excellent","band","tonight"]
+
+once we have the array of strings representing the user's input sentence, we
+loop through each words, find the key marching the word and returns back the value tied to that
+word in our dictionary.
+
+After each word is translated, we then
+print out the translated sentence to the user.
+"""
+
+"""
+main():
+  set sentence = input()
+  set dictionary = create_dictionary()
+  translate(sentence, dictionary)
+
+ translate(sentence, dictionary):
+  words = for each word in the sentence
+  for each words, translate the word
+  print translated sentence to user
+
+ create_dictionary():
+  read in textese.txt
+  create list = each line from file
+  close the file
+  create a dict off the list
+  return the dict
+
+ main()
+ """
+
+def main():
+  sentence = input("Enter a sentence ")
+  dictionary = create_dictionary("textese.txt")
+  translate(sentence, dictionary)
+
+def create_dictionary(txt_file):
+  infile = open(txt_file, "r")
+  words = [word.rstrip() for word in infile]
+  infile.close()
+  return dict({word.split(",")for word in words})
+
+def translate(sentence, dictionary):
+  words = sentence.split()
+  for word in words:
+    print(dictionary.get(word, word)," ", end="")          
+main()
+
+
+
+
